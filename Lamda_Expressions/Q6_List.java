fdd49653cf5a5db36fd5e82fa725119aae40c00f
Adikumkar <103571396+Adikumkar@users.noreply.github.com>
2022-06-24 11:24:23 +0530

    Add files via upload

---
+++ Lamda_Expressions/Q6_List.java
@@ -0,0 +1,19 @@
+package Lamda_Expressions;
+
+import java.util.ArrayList;
+
+public class Q6_List {
+public static void main(String[] args) {
+	ArrayList<String> a=new ArrayList<>();
+	a.add("Virat");
+	a.add("Virat");
+	a.add("Dhoni");
+	a.add("Rishabh");
+	a.add("Warner");
+	a.add("Miller");
+	a.add("Bravo");
+	a.add("Pollard");
+	a.add("Southee");
+	a.add("Boult");
+}
+}
---
+++ Lamda_Expressions/Unary_Lamda.java
@@ -0,0 +1,7 @@
+package Lamda_Expressions;
+
+public class Unary_Lamda {
+public static void main(String[] args) {
+	UnaryOperator<Boolean> op=new UnaryO
+}
+}
